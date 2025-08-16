40ac7600ab9711682868e4bdd31147517527c05d
Marco Virgili <marco.virgili15@gmail.com>
2025-08-16 17:48:41 +0100

    Improved layout

--- calculators/mortgage_broker.py
+++ calculators/mortgage_broker.py
@@ -6,13 +6,17 @@ import streamlit as st
 from src.interest_utils import calculate_rate_with_broker
 
 st.title("Is a mortgage broker worth it?")
+st.subheader("Find the minimum interest rate a broker must save you to justify their fee.")
 
-home_price = st.number_input("Home Price (£)", value=300000, step=500)
-deposit = st.number_input("Deposit (£)", value=30000, step=1)
-mortgage_duration_years = st.number_input("Mortgage term (years)", value=25, step=1)
-interest_rate_perc = st.number_input("Interest rate you could get on your own (%)", value=4.5, step=0.01)
-fixed_term_duration = st.number_input("Duration of fixed rate term (Years)", value=5, step=1)
-broker_fee = st.number_input("Broker's fee (£)", value=500, step=1)
+col1, col2 = st.columns(2)
+with col1:
+    home_price = st.number_input("🏠 Home Price (£)", value=300000.0, step=0.01)
+    deposit = st.number_input("💰 Deposit (£)", value=30000.0, step=0.01)
+    mortgage_duration_years = st.number_input("⏳ Mortgage term (years)", value=25, step=1)
+with col2:
+    interest_rate_perc = st.number_input("📈 Interest rate you could get on your own (%)", value=4.5, step=0.01)
+    fixed_term_duration = st.number_input("📅 Duration of fixed rate term (Years)", value=5, step=1)
+    broker_fee = st.number_input("💸 Broker's fee (£)", value=500.0, step=0.01)
 
 
 if st.button("Calculate"):
@@ -28,4 +32,6 @@ if st.button("Calculate"):
                                                       fixed_term_duration,
                                                       interest_rate_perc)
 
-        st.success(f"Interest rate a broker should get you to balance the fee: **{rate_with_broker:,.2f}%**")
+        st.success(f"The broker should get you an interest rate of **{rate_with_broker:,.2f}%** "
+                   f"to save you £{broker_fee:,.2f} in interest over the initial {fixed_term_duration} years period, "
+                   f"assuming you could get a rate of {interest_rate_perc}% without them.")
